fbeac0e7740ffb98dfbc4ce4ba7493d9146daf9a
Francesco Murdaca <fmurdaca@redhat.com>
2020-11-26 08:39:30 +0100

    Pre-commit (#24)
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
-"""
-jupyterlab-requirements setup
-"""
+"""jupyterlab-requirements setup."""
 import json
 import os
 
 from pathlib import Path
-from glob import glob
 
 from jupyter_packaging import (
     create_cmdclass, install_npm, ensure_targets,
-    combine_commands, get_version,
+    combine_commands
 )
 import setuptools
 
@@ -41,13 +38,16 @@ labext_name = "jupyterlab_requirements"
 data_files_spec = [
     ("share/jupyter/lab/extensions%s" % labext_name, lab_path, "**"),
     ("share/jupyter/lab/extensions%s" % labext_name, HERE, "install.json"),
-    ("etc/jupyter/jupyter_server_config.d", "jupyter-config/jupyter_server_config.d", "jupyterlab_requirements.json"),
-    ("etc/jupyter/jupyter_notebook_config.d", 'jupyter-config/jupyter_notebook_config.d', 'jupyterlab_requirements.json')
+    ("etc/jupyter/jupyter_server_config.d",
+        "jupyter-config/jupyter_server_config.d", "jupyterlab_requirements.json"),
+    ("etc/jupyter/jupyter_notebook_config.d",
+        'jupyter-config/jupyter_notebook_config.d', 'jupyterlab_requirements.json')
 ]
 
 # To deploy simultaneously the frontend and the backend,
 # the frontend NPM package needs to be built and inserted in the Python package.
-cmdclass = create_cmdclass("jsdeps",
+cmdclass = create_cmdclass(
+    "jsdeps",
     package_data_spec=package_data_spec,
     data_files_spec=data_files_spec
 )
@@ -55,17 +55,20 @@ cmdclass = create_cmdclass("jsdeps",
 cmdclass["jsdeps"] = combine_commands(
     # it will build the frontend NPM package
     install_npm(HERE, build_cmd="build:prod", npm=["jlpm"]),
-    # It will copy the NPM package in the Python package 
-    # and force it to be copied in a place JupyterLab is looking for frontend extensions when the Python package is installed.
+    # It will copy the NPM package in the Python package
+    # and force it to be copied in a place JupyterLab
+    # is looking for frontend extensions when the Python package is installed.
     ensure_targets(jstargets),
 )
 
 README: str = Path(HERE, "README.md").read_text(encoding="utf-8")
 
-def get_install_requires():
+
+def _get_install_requires():
     with open('requirements.txt', 'r') as requirements_file:
         res = requirements_file.readlines()
         return [req.split(' ', maxsplit=1)[0] for req in res if req]
+
 
 setup_args = dict(
     name=name,
@@ -77,7 +80,7 @@ setup_args = dict(
     long_description= README,
     long_description_content_type="text/markdown",
     cmdclass=cmdclass,
-    install_requires=get_install_requires(),
+    install_requires=_get_install_requires(),
     zip_safe=False,
     include_package_data=True,
     python_requires=">=3.8",
