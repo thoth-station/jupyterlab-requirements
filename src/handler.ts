93f7d450949161d437b7d7203a3561553dae549a
Francesco Murdaca <fmurdaca@redhat.com>
2020-11-26 09:37:26 +0100

    Introduce handler for frontend
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

---
+++ src/handler.ts
@@ -0,0 +1,58 @@
+/**
+ * Jupyterlab requirements.
+ *
+ * Jupyterlab extension for managing dependencies.
+ *
+ * @link   https://github.com/thoth-station/jupyterlab-requirements#readme
+ * @file   Jupyterlab extension for managing dependencies.
+ * @author Francesco Murdaca <fmurdaca@redhat.com>
+ * @since  0.0.1
+ */
+/**
+ * Jupyterlab tutorials
+ */
+
+import { URLExt } from '@jupyterlab/coreutils';
+
+import { ServerConnection } from '@jupyterlab/services';
+
+/**
+ * Call the API extension
+ *
+ * @param endPoint API REST end point for the extension
+ * @param init Initial values for the request
+ * @returns The response body interpreted as JSON
+ */
+export async function requestAPI<T>(
+  endPoint = '',
+  init: RequestInit = {}
+): Promise<T> {
+  // Make request to Jupyter API
+
+  // Retrieve server setting
+  const settings = ServerConnection.makeSettings();
+
+  // build the full request URL
+  const requestUrl = URLExt.join(
+    settings.baseUrl,
+    'jupyterlab-requirements',
+    endPoint
+  );
+
+  let response: Response;
+
+  // Make actual request
+  try {
+    response = await ServerConnection.makeRequest(requestUrl, init, settings);
+  } catch (error) {
+    throw new ServerConnection.NetworkError(error);
+  }
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new ServerConnection.ResponseError(response, data.message);
+  }
+
+  return data;
+}
