9dfc7e1806d092f96583fcc8a069de140efbed40
Francesco Murdaca <fmurdaca@redhat.com>
2020-11-26 10:42:01 +0100

    Add interfaces for requirements and thoth (#36)
    * Add interfaces for requirements
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>
    
    * Add interfaces for Thoth
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

---
+++ src/types/requirements.ts
@@ -0,0 +1,70 @@
+/**
+ * Jupyterlab requirements.
+ *
+ * Jupyterlab extension for managing dependencies.
+ *
+ * @link   https://github.com/thoth-station/jupyterlab-requirements#readme
+ * @file   Jupyterlab extension for managing dependencies.
+ * @author Francesco Murdaca <fmurdaca@redhat.com>
+ * @since  0.0.1
+ */
+
+
+// Requirements
+
+export interface PythonPackage {
+    [ name: string ]: string
+}
+
+export interface Requires {
+    python_version: string
+}
+
+export interface SourcesEntity {
+    name: string
+    url: string
+    verify_ssl: boolean
+}
+
+export interface Requirements {
+    aliases: { [ name: string ]: string }
+    packages: Array<PythonPackage>
+    requires: Requires
+    sources: ( SourcesEntity )[] | null
+    "dev-packages"?: any
+}
+
+// Requirements Lock
+export interface Hash {
+    sha256: string
+}
+
+export interface Meta {
+    hash: Hash
+    requires: Requires
+    sources: ( SourcesEntity )[] | null
+    "pipfile-spec"?: number
+}
+
+export interface LockedPackageVersion {
+    version: string
+    hashes: ( string )[] | null
+    index?: string | null
+    markers?: string | null
+}
+
+export interface RequirementLock {
+    _meta: Meta
+    default: { [ name: string ]: LockedPackageVersion }
+    develop: { [ name: string ]: LockedPackageVersion }
+}
+
+export class Source {
+    constructor(
+        public readonly name: string = "pypi",
+        public readonly url: string = "https://pypi.org/simple",
+        public readonly verify_ssl: boolean = true,
+        public warehouse?: string,
+        public warehouse_api_url?: string
+    ) { }
+}
---
+++ src/types/thoth.ts
@@ -0,0 +1,41 @@
+/**
+ * Jupyterlab requirements.
+ *
+ * Jupyterlab extension for managing dependencies.
+ *
+ * @link   https://github.com/thoth-station/jupyterlab-requirements#readme
+ * @file   Jupyterlab extension for managing dependencies.
+ * @author Francesco Murdaca <fmurdaca@redhat.com>
+ * @since  0.0.1
+ */
+
+import { Requirements, RequirementLock } from "./requirements";
+
+
+export interface Advise {
+    error: boolean,
+    requirements: Requirements,
+    requirement_lock: RequirementLock
+}
+
+// Thoth config
+
+export interface OperatingSystem {
+    name: string,
+    version: string,
+}
+
+export interface RuntimeEnvornment {
+    name: string,
+    operating_system: OperatingSystem
+    python_version: string,
+    recommendation_type: string
+
+}
+
+export interface ThothConfig {
+    host: string,
+    tls_verify: boolean,
+    requirements_format: string,
+    runtime_environments: [ RuntimeEnvornment ]
+}
