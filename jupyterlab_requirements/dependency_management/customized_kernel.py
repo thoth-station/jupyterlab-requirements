f40abbe356aec68cb598c160fe38e38f28e76ae0
Francesco Murdaca <fmurdaca@redhat.com>
2020-12-07 11:30:11 +0100

    Adjust method to create kernel locally
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- jupyterlab_requirements/dependency_management/customized_kernel.py
+++ jupyterlab_requirements/dependency_management/customized_kernel.py
@@ -16,8 +16,13 @@
 
 """Create new kernel API for jupyterlab requirements."""
 
+import os
 import json
 import logging
+import subprocess
+
+from pathlib import Path
+from daiquiri import output
 
 from jupyter_server.base.handlers import APIHandler
 from tornado import web
@@ -27,22 +32,40 @@ from ipykernel.kernelspec import install
 _LOGGER = logging.getLogger("jupyterlab_requirements.customized_kernel")
 
 
-class CustomizedKernelHandler(APIHandler):
-    """Customized Kernel handler to create new kernel for jupyter."""
+class JupyterKernelHandler(APIHandler):
+    """Jupyter Kernel handler to create new kernel for notebooks."""
 
     @web.authenticated
     def post(self):
-        """Install packages using selected package manager."""
+        """Create kernel using new virtual environment."""
+        initial_path = Path.cwd()
         input_data = self.get_json_body()
 
+        notebook_path: str = input_data["notebook_path"]
         kernel_name: str = input_data["kernel_name"]
 
-        # TODO: Check if ipykernel is installed
+        complete_path = initial_path.joinpath(Path(notebook_path).parent)
+        os.chdir(os.path.dirname(complete_path))
+
+        # TODO: Check if ipykernel is installed, otherwise install it
+        process_output = subprocess.run(f". {kernel_name}/bin/activate && pip show ipykernel", shell=True, capture_output=True, cwd=complete_path)
+        print(process_output.stdout)
+        process_output = subprocess.run(f". {kernel_name}/bin/activate && pip install ipykernel", shell=True, cwd=complete_path)
 
         _LOGGER.debug(f"Installing kernelspec called {kernel_name}." )
-        kernel_path = install(kernel_name=kernel_name, user=True)
 
+        try:
+            # Create new kernel
+            # process_output = subprocess.call(
+            #     f". {kernel_name}/bin/activate && ./{kernel_name}/bin/python3"
+            #     f" -m ipykernel install --user --name={kernel_name} --display-name 'Python ({kernel_name})'", shell=True, cwd=complete_path)
+            process_output = subprocess.run(f". {kernel_name}/bin/activate && ipython kernel install --user --name={kernel_name} --display-name 'Python ({kernel_name})'", shell=True, cwd=complete_path)
+
+        except Exception as e :
+            print(f"Could not enter environment {e}")
+
+
+        os.chdir(initial_path)
         self.finish(json.dumps({
-            "data": f"installed kernel {kernel_name} at {kernel_path}"
+            "data": f"installed kernel {kernel_name} at {complete_path}"
         }))
-        self.flush()
