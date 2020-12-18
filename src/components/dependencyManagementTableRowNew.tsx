b9fb33b16db71a805fd0ba6300a0a1e56c4c954b
Francesco Murdaca <fmurdaca@redhat.com>
2020-12-18 19:38:30 +0100

    Allow user to select recommendation type for thoth advise (#67)
    * Introduce dropdown for recommendation type for user
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>
    
    * Make pre-commit happy
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- src/components/dependencyManagementTableRowNew.tsx
+++ src/components/dependencyManagementTableRowNew.tsx
@@ -199,7 +199,6 @@ export class DependencyManagementTableRowNew extends React.Component<IProps, ISt
                         </button>;
         }
 
-
         return (
             <tr>
                 <td>
