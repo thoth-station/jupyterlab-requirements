19895b81c0a878850a27e9afcd3f29c64bb9e155
Francesco Murdaca <fmurdaca@redhat.com>
2021-02-04 20:01:56 +0100

    Use correct package name
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- jupyterlab_requirements/__init__.py
+++ jupyterlab_requirements/__init__.py
@@ -54,7 +54,7 @@ def _load_jupyter_server_extension(server_app):
     base_url = web_app.settings["base_url"]
     print("base_url:", base_url)
 
-    url_path = "jupyterlab-requirements"
+    url_path = "jupyterlab_requirements"
 
     # Prepend the base_url so that it works in a jupyterhub setting
     handlers = [
