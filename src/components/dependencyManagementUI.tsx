91269b270c90ca06a5689d74710796312abcd0e6
Francesco Murdaca <fmurdaca@redhat.com>
2021-02-22 16:45:26 +0100

    Fix/match packages check to verify the kernel packages matches the pipfile.lock in user notebook (#150)
    * Fix check on packages installed
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>
    
    * Correct message
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>
    
    * Use name and version in check
    
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- src/components/dependencyManagementUI.tsx
+++ src/components/dependencyManagementUI.tsx
@@ -930,7 +930,7 @@ export class DependenciesManagementUI extends React.Component<IProps, IState> {
         console.log(initial_locked_packages)
 
         // Retrieve kernel name from metadata
-        const kernel_name = get_kernel_name( this.props.panel)
+        const kernel_name = get_kernel_name( this.props.panel )
 
         // Check if all package in requirements are also in requirements locked (both from notebook metadata)
         const check_packages = {}
@@ -958,7 +958,6 @@ export class DependenciesManagementUI extends React.Component<IProps, IState> {
 
         if (_.isEqual(_.size(initial_packages), _.size(check_packages) )) {
 
-
           // check if all requirements locked are also installed in the current kernel
           const are_installed: boolean = await this.checkInstalledPackages(installed_packages, initial_locked_packages)
 
@@ -979,7 +978,7 @@ export class DependenciesManagementUI extends React.Component<IProps, IState> {
             return
           }
 
-          // if locked requirements are not present or not all present in the kernel, go to only_install state
+          // if locked requirements are not present or not all present in the kernel, go to only_install_kernel state
           else {
             this.changeUIstate(
               "only_install_kernel",
@@ -1033,15 +1032,18 @@ export class DependenciesManagementUI extends React.Component<IProps, IState> {
 
     async checkInstalledPackages(installed_packages: {}, packages: {}): Promise<boolean> {
 
-      // Check installed packages
-      if (_.isEqual(_.size(packages), _.size(installed_packages) )) {
-        return true
-      }
+      // Check installed packages and verify pipfile.lock are all in installed list
+      _.forIn(packages, function(version, name) {
+        if (_.has(installed_packages, name.toLowerCase()) && _.get(installed_packages, name.toLowerCase()) == version ) {
+            console.log( `Package '${ name }' in version '${ version }' is already installed` )
+        }
+        else {
+          return false
+        }
+        }
+      )
 
-      else {
-        return false
-      }
-
+      return true
     }
 
     async createConfig() {
@@ -1323,7 +1325,7 @@ export class DependenciesManagementUI extends React.Component<IProps, IState> {
         return (
           <div>
             <fieldset>
-              <p>Thoth resolution engine failed... pipenv will be used to lock and install dependencies!</p>
+              <p>Thoth resolution engine failed... pipenv will be used to lock dependencies!</p>
             </fieldset>
           </div>
       );
